661febd7d6caa8dbf471e2477257cc53295a0a6a
thexilent13 <jennicaalcalde@yahoo.com>
2015-04-04 19:16:28 +0800

    working login, signup, logout, avail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :require_login
 
   def index
+    @current_user = current_user
 
   end
 
@@ -10,9 +11,14 @@ class UsersController < ApplicationController
     plan.name = params[:name]
     plan.price = params[:price]
     plan.duration = params[:duration]
+    plan.user_id = session[:user_id]
 
     if(plan.name.present? && plan.price.present? && plan.duration.present?)
       plan.save
+
+      current_user_edit = current_user
+      current_user_edit.plan_id = plan.id
+      current_user_edit.save
     end
 
     redirect_to home_path
