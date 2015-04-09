c2e3fd251c00b1a0541509e1c4398e095c74cde1
thexilent13 <jennicaalcalde@yahoo.com>
2015-04-09 12:13:03 +0800

    change the db for user, model, and transaction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,30 +20,32 @@ class UsersController < ApplicationController
   end
 
   def avail_plan
-    if current_user.plan_id.nil?
-      # plan = Plan.new
-      # plan.name = params[:name]
-      # plan.price = params[:price]
-      # plan.duration = params[:duration]
-      # plan.user_id = session[:user_id]
+    # if current_user.plan_id.nil?
+    #   plan = Plan.new
+    #   plan.name = params[:name]
+    #   plan.price = params[:price]
+    #   plan.duration = params[:duration]
+    #   plan.user_id = session[:user_id]
+    #
+    #   if(plan.name.present? && plan.price.present? && plan.duration.present?)
+    #     # save plan to the database
+    #     plan.save
+    #
+    #     # save the plan_id to the user
+    #     current_user.update_column(:plan_id, plan.id)
+    #
+    #     # deliver the avail plan confirmation email
+    #     UserMailer.avail_plan_confirmation(current_user, plan).deliver
+    #   end
+    # else
+    #   change_current_plan = Plan.where(user_id: current_user.id).first
+    #   change_current_plan.update(name: params[:name], price: params[:price], duration: params[:duration])
+    #
+    #   # deliver the avail plan confirmation email
+    #   UserMailer.avail_plan_confirmation(current_user, change_current_plan).deliver
+    # end
 
-      if(plan.name.present? && plan.price.present? && plan.duration.present?)
-        # save plan to the database
-        plan.save
-
-        # save the plan_id to the user
-        current_user.update_column(:plan_id, plan.id)
-
-        # deliver the avail plan confirmation email
-        UserMailer.avail_plan_confirmation(current_user, plan).deliver
-      end
-    else
-      change_current_plan = Plan.where(user_id: current_user.id).first
-      change_current_plan.update(name: params[:name], price: params[:price], duration: params[:duration])
-
-      # deliver the avail plan confirmation email
-      UserMailer.avail_plan_confirmation(current_user, change_current_plan).deliver
-    end
+    if current_user_transaction.nil?
 
     redirect_to home_path
   end
